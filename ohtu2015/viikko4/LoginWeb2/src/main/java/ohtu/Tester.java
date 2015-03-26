7ff90b54c663bcacffaea27086e900759f07614c
Pihla Toivanen <toivanenpihla@gmail.com>
2015-03-26 20:18:27 +0200

    Viikko4 Easyb - Selenium testit

--- ohtu2015/viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ ohtu2015/viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -11,18 +11,26 @@ public class Tester {
 
         driver.get("http://localhost:8090");
         System.out.println( driver.getPageSource() );
-        WebElement element = driver.findElement(By.linkText("login"));       
+        WebElement element = driver.findElement(By.linkText("register new user"));       
         element.click(); 
         
         System.out.println("==");
         
         System.out.println( driver.getPageSource() );
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("pihlat");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
+        element.sendKeys("akkeadsasdp2");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("akkeadsasdp2");
+        element = driver.findElement(By.name("add"));
         element.submit();
+        
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        
+         element = driver.findElement(By.linkText("logout"));
+        element.click();
         
         System.out.println("==");
         System.out.println( driver.getPageSource() );
